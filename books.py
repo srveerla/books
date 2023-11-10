502a54a939c95c916c4348b2d14365fcfcff117e
srveerla <39939637+srveerla@users.noreply.github.com>
2023-11-10 19:36:20 +0530

    Update books.py

--- books.py
+++ books.py
@@ -2,16 +2,16 @@ from flask import Flask, jsonify
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/users_db'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/books_db'
 mongo = PyMongo(app)
 
-@app.route('/users/<username>', methods=['GET'])
-def get_user(username):
-    user = mongo.db.users.find_one({'username': username})
-    if user:
-        return jsonify({"username": user['username'], "email": user['email']})
+@app.route('/books/<book_title>', methods=['GET'])
+def get_book(book_title):
+    book = mongo.db.books.find_one({'title': book_title})
+    if book:
+        return jsonify({"title": book['title'], "author": book['author']})
     else:
-        return jsonify({"error": "User not found"}), 404
+        return jsonify({"error": "Book not found"}), 404
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5001)
