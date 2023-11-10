c3ea77ab859f00ac2d5fc42dad80e4690810a0ef
srveerla <39939637+srveerla@users.noreply.github.com>
2023-11-10 19:35:11 +0530

    Update books.py

--- books.py
+++ books.py
@@ -1,23 +1,17 @@
 from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
-db = SQLAlchemy(app)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/users_db'
+mongo = PyMongo(app)
 
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    author = db.Column(db.String(50), nullable=False)
-
-@app.route('/books/<int:book_id>', methods=['GET'])
-def get_book(book_id):
-    book = Book.query.get(book_id)
-    if book:
-        return jsonify({"id": book.id, "title": book.title, "author": book.author})
+@app.route('/users/<username>', methods=['GET'])
+def get_user(username):
+    user = mongo.db.users.find_one({'username': username})
+    if user:
+        return jsonify({"username": user['username'], "email": user['email']})
     else:
-        return jsonify({"error": "Book not found"}), 404
+        return jsonify({"error": "User not found"}), 404
 
 if __name__ == '__main__':
-    db.create_all()
-    app.run(port=5001)
+    app.run(port=5000)
