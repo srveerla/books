4d19d0123059a69849ac1f4f63d6554ba4a7f8cf
srveerla <39939637+srveerla@users.noreply.github.com>
2023-11-11 16:08:35 +0530

    Update books.py

--- books.py
+++ books.py
@@ -1,17 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/books_db'
+app.config["MONGO_URI"] = "mongodb://mongo:27017/library"
 mongo = PyMongo(app)
 
-@app.route('/books/<book_title>', methods=['GET'])
-def get_book(book_title):
-    book = mongo.db.books.find_one({'title': book_title})
-    if book:
-        return jsonify({"title": book['title'], "author": book['author']})
-    else:
-        return jsonify({"error": "Book not found"}), 404
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+@app.route('/books', methods=['GET'])
+def get_books():
+    books = list(mongo.db.books.find({}, {'_id': 0}))
+    return jsonify(books)
+
+@app.route('/books', methods=['POST'])
+def add_book():
+    data = request.get_json()
+    book = Book(data['title'], data['author'])
+    mongo.db.books.insert_one(book.__dict__)
+    return 'Book added', 201
 
 if __name__ == '__main__':
-    app.run(port=5001)
+    app.run(host='0.0.0.0')
